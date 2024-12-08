55136464d7047bdfb3b11fc87b71d4819b62650c
vol1ng <91206572+vol1ng@users.noreply.github.com>
2024-12-08 18:19:38 +0400

    Update main.js

--- main.js
+++ main.js
@@ -46,7 +46,7 @@ loader.style.display = "block";
 setTimeout(() => {
   loader.style.display = "none";
   content.style.display = "block";
-}, 4000);
+}, 1000);
 
 document.getElementById("wordInput").addEventListener("input", function () {
   const query = this.value.trim().toLowerCase();
